d0bc495fb831200901046a1ca2641d09e1c539e7
juicybob123 <okruh2028@kehillahstudent.org>
2026-04-16 23:56:01 -0700

    aaa

--- q1.py
+++ q1.py
@@ -7,3 +7,13 @@ ella = Cat()
 zoe = Cat()
 print(ella)
 print(zoe)
+class Dog:
+    """a dog"""
+    def speak():
+        print("Woof!")
+niki= Dog()
+Muki = Dog()
+Blue = Dog()
+print(niki)
+print(Muki)
+print(Blue)
--- q3.py
+++ q3.py
@@ -5,7 +5,19 @@ class Cat:
     def __str__(self):
         return f"a Cat names {self.name}"
     def speak(self):
-        print(f"{sefl.name} says meow!")
+        print(f"{self.name} says meow!")
 
 ella = Cat("Ella")
 ella.speak()
+class dog:
+    """a dog"""
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"a Dog names {self.name}"
+    def speak(self):
+        print(f"{self.name} says Woof!")
+
+muki = dog("muki")
+muki.speak()
+
